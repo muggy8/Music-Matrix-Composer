de2f7caacdace67a898c776c02d5ddd0db15658c
muggy8 <muggy8@gmail.com>
2015-10-09 15:57:23 -0700

    added effectient object to JSON song data

--- mmc/JS/MMC2.js
+++ mmc/JS/MMC2.js
@@ -1225,6 +1225,24 @@ var song={
     "track14":{"id":"", "instrument":"", "songData":[], "scale":"", "lastVolKey":0.5}, 
     "track15":{"id":"", "instrument":"", "songData":[], "scale":"", "lastVolKey":0.5}, 
     "metaData":{"length":"", "name":"", "nps":"", "songID":""}
+	"player":{
+		"track0Data":[],
+		"track1Data":[],
+		"track2Data":[],
+		"track3Data":[],
+		"track4Data":[],
+		"track5Data":[],
+		"track6Data":[],
+		"track7Data":[],
+		"track8Data":[],
+		"track9Data":[],
+		"track10Data":[],
+		"track11Data":[],
+		"track12Data":[],
+		"track13Data":[],
+		"track14Data":[],
+		"track15Data":[],
+	}
 };
 
 var combos = [];
